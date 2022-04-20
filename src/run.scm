8a4569ed0c37e05ebc7e894a18214f37a12bfa5d
Tong Zhao <tongzhao997@gmail.com>
2022-04-20 02:16:09 -0400

    Fixed load.scm; run.scm works now

--- src/run.scm
+++ src/run.scm
@@ -1,3 +1,19 @@
-(with-working-directory-pathname
- (directory-pathname (current-load-pathname))
- (load "load"))
+
+(load "load")
+;; (load "~/Dropbox/classes/6.945/6945-final-project/src/load.scm")
+
+(define bob (node:create 'name 'bobs-name))
+
+(write (get-name bob))
+(newline)
+(write (get-name bob))
+(newline)
+(write (get-name bob))
+(newline)
+(write (get-name bob))
+(newline)
+(write (get-name bob))
+(newline)
+
+
+
