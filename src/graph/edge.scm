528a4ca2d9fb1f049f34dc3dc92c50260f2b1128
alexjli <alexjli@mit.edu>
2022-04-24 23:00:39 -0400

    super quick crude edge implementation

---
+++ src/graph/edge.scm
@@ -0,0 +1,48 @@
+#| Edges
+
+TODO : docs
+|#
+
+(define (edge-id? x)
+  (or (string? x) (exact-nonnegative-integer? x)))
+(register-predicate! edge-id? 'edge-id)
+
+(define edge:id
+  (make-property 'id
+                 'predicate edge-id?
+                 'default-value '()))
+
+(define edge:data
+  (make-property 'data
+                 'predicate object?
+                 'default-value '()))
+
+(define edge:source
+  (make-property 'source
+                 'predicate node?))
+
+(define edge:destination
+  (make-property 'destination
+                 'predicate node?))
+
+(define edge?
+  (make-type 'edge (list edge:source edge:destination edge:data edge:id)))
+(set-predicate<=! edge? object?)
+
+(define edge:get-id
+  (property-getter edge:id edge?))
+
+(define edge:get-data
+  (property-getter edge:data edge?))
+
+(define edge:get-source
+  (property-getter edge:source edge?))
+
+(define edge:get-destination
+  (property-getter edge:destination edge?))
+
+(define edge:set-data
+  (property-setter edge:data edge? object?))
+
+(define edge:create
+  (type-instantiator edge?))
