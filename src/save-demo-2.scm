073255c849dfb75715865b70a82b71e65ed419fe
alexjli <alexjli@mit.edu>
2022-05-06 12:28:02 -0400

    change to pretty print for demo

--- src/save-demo-2.scm
+++ src/save-demo-2.scm
@@ -5,36 +5,32 @@
 
 ;; Everything is present!
 
-(write-line (node:get-id node-1))
-(write-line (node:get-label node-1))
-(write-line (node:get-data node-1))
-(newline)
-(newline)
+(begin
+  (newline)
+  (pp node-1)
+  (newline))
 
-(write-line (node:get-id node-2))
-(write-line (node:get-label node-2))
-(write-line (node:get-data node-2))
-(newline)
-(newline)
+(begin
+  (newline)
+  (pp node-2)
+  (newline))
 
-(write-line (node:get-id node-3))
-(write-line (node:get-label node-3))
-(write-line (node:get-data node-3))
-(newline)
-(newline)
+(begin
+  (newline)
+  (pp node-3)
+  (newline))
 
-(write-line (edge:get-id edge-4))
-(write-line (edge:get-label edge-4))
-(write-line (edge:get-data edge-4))
-(newline)
-(newline)
+(begin
+  (newline)
+  (pp edge-4)
+  (newline))
 
-(write-line (edge:get-id edge-5))
-(write-line (edge:get-label edge-5))
-(write-line (edge:get-data edge-5))
-(newline)
-(newline)
+(begin
+  (newline)
+  (pp edge-5)
+  (newline))
 
+(pp graph-name-placeholder)
 (write-line (graph:get-nodes graph-name-placeholder))
 (write-line (graph:get-edges graph-name-placeholder))
 
